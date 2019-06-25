0a39fa566ea084cd8ee344966a420362c593b70c
Jürgen Löhel <jloehel@suse.com>
2019-06-25 15:19:07 +0200

    Adds the messaging between content and background scripts.
    Signed-off-by: Jürgen Löhel <jloehel@suse.com>

--- src/hackweek/background_script.js
+++ src/hackweek/background_script.js
@@ -1,6 +1,75 @@
-const leftPad = require("left-pad");
+'use strict'
 
-browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    const result = leftPad(message.text, message.amount, message.with);
-    sendResponse(result);
+
+console.log("background-script: LOAD");
+
+var connections = {};
+
+// https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/runtime/onConnect
+// Supported by Chrome, Edge, Firefox and Opera
+browser.runtime.onConnect.addListener(function (port) {
+  console.log("background-script onConnect", port);
+
+  // Only accept connections from panel or content scripts
+  if (port.name != "panel" && port.name != "content") {
+    return;
+  }
+
+  var extensionListener = function (message) {
+    console.log("background-script port.onMessage", message, port);
+
+    var tabId = port.sender.tab ? port.sender.tab.id : message.tabId;
+
+    if (message.action == "init") {
+      if (!connections[tabId]) {
+        connections[tabId] = {};
+      }
+      connections[tabId][port.name] = port;
+      return;
+    }
+
+    if (message.target) {
+      var conn = connections[tabId][message.target];
+      if (conn) {
+        conn.postMessage(message);
+      }
+    }
+  };
+  port.onMessage.addListener(extensionListener);
+
+  port.onDisconnect.addListener(function(port) {
+    console.log("background-script onDisconnect", port);
+
+    port.onMessage.removeListener(extensionListener);
+
+    var tabs = Object.keys(connections);
+    for (var i=0, len=tabs.length; i < len; i++) {
+      if (connections[tabs[i]][port.name] === port) {
+        console.log("background-script onDisconnect connections cleanup",
+                    {tabId: tabs[i], portName: port.name});
+        delete connections[tabs[i]][port.name];
+
+        if (Object.keys(connections[tabs[i]]).length === 0) {
+          console.log("background-script onDisconnect remove connection object",
+                      {tabId: tabs[i]});
+          delete connections[tabs[i]];
+        }
+        break;
+      }
+    }
+  });
+});
+
+// https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/runtime/onMessage
+// Supported by Chrome, Edge, Firefox and Opera
+browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  console.log("background-script runtime.onMessage", request, sender);
+
+  if (request.target == "content" && request.tabId) {
+    //https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/sendMessage
+    // Supported by Chrome, Edge, Firefox and Opera
+    browser.tabs.sendMessage(request.tabId, request);
+  }
+
+  return true;
 });
--- src/hackweek/content_script.js
+++ src/hackweek/content_script.js
@@ -1,1 +1,39 @@
-document.body.style.border = "5px solid red";
+// Content scripts have access to the DOM
+// The content scripts are available in every tab
+// The content script needs to send the DOM to the background script.
+//
+
+'use strict'
+
+
+console.log("content-script: LOAD");
+
+var port;
+
+function setupPort() {
+  console.log("content-script: Setting up port")
+  if (!port) {
+    console.log("content-script: New port")
+    // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/runtime/connect
+    port = browser.runtime.connect("web_extension@loehel.de", { name: "content" });
+    // returns a runtime.Port
+    // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/runtime/Port
+    console.log("content-script: Sending init message to the background")
+    port.postMessage({ action: "init" });
+    port.onDisconnect.addListener(function () {
+      port = null;
+    });
+  }
+}
+
+function getDOM() {
+  console.log("content-script: Getting DOM")
+  setupPort();
+  console.log("content-script: Sending DOM message to the background")
+  port.postMessage({
+    action: "extractDOM",
+    dom: document.documentElement.innerHTML,
+  });
+};
+
+getDOM();
